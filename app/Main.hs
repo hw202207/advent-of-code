a746b3549d69058e8e99b3270996f7be273c4313
Haisheng W - M <haisheng@mercury.com>
2023-12-05 17:22:50 -0800

    day 1 part 2

--- app/Main.hs
+++ app/Main.hs
@@ -5,4 +5,4 @@ import qualified Day1
 main :: IO ()
 main = do
   putStrLn "Hello, Advent of code 2023!"
-  Day1.main
+  Day1.go2
--- src/Day1.hs
+++ src/Day1.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE ImportQualifiedPost #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module Day1 where
 
@@ -32,37 +33,20 @@ calibrateStr str =
             1 -> read $ T.unpack $ xs <> xs
             _ -> read (T.head xs : T.last xs : [])
 
--- numberStrToNumber :: Text -> Text
--- numberStrToNumber input =
---   let result = foldr
---                 (\(str, num) val ->
---                    if T.take (T.length str) input == str
---                    then val <> (T.pack $ show num) <> numberStrToNumber (T.drop (T.length str) input)
---                    else val
---                 )
---                 ""
---                 numberStr
---    in
---     if result == "" then (T.take 1 input <> numberStrToNumber (T.drop 1 input) ) else result
-
 numberStrToNumber :: Text -> Text
-numberStrToNumber str
-    | T.length str < 3 = str
+numberStrToNumber input
+    | T.null input = input
     | otherwise =
-        let (x, i) = case T.take 3 str of
-                "one" -> ("1", 3)
-                "two" -> ("2", 3)
-                "six" -> ("6", 3)
-                _ -> case T.take 4 str of
-                    "four" -> ("4", 4)
-                    "five" -> ("5", 4)
-                    "nine" -> ("9", 4)
-                    _ -> case T.take 5 str of
-                        "three" -> ("3", 5)
-                        "eight" -> ("8", 5)
-                        "seven" -> ("7", 5)
-                        _ -> (T.take 1 str, 1)
-         in x <> numberStrToNumber (T.drop i str)
+        let result =
+                foldr
+                    ( \(str, num) val ->
+                        if T.take (T.length str) input == str
+                            then T.pack (show num)
+                            else val
+                    )
+                    (T.take 1 input)
+                    numberStr
+         in result <> numberStrToNumber (T.drop 1 input)
 
 numberStr :: [(Text, Int)]
 numberStr =
@@ -77,12 +61,17 @@ numberStr =
     , ("nine", 9)
     ]
 
--- T.replace "nine" "9"
---     . T.replace "eight" "8"
---     . T.replace "seven" "7"
---     . T.replace "six" "6"
---     . T.replace "five" "5"
---     . T.replace "four" "4"
---     . T.replace "three" "3"
---     . T.replace "two" "2"
---     . T.replace "one" "1"
+test2 :: IO ()
+test2 =
+    print
+        $ sum
+        $ map (calibrateStr . numberStrToNumber)
+        $ [ "two1nine"
+          , "eightwothree"
+          , "abcone2threexyz"
+          , "xtwone3four"
+          , "4nineeightseven2"
+          , "zoneight234"
+          , "7pqrstsixteen"
+          , "3onefqltjzdrfourcpkfhceightwomc" -- shall be 32
+          ]
