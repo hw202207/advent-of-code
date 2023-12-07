775cee354c471c2bda689d5df3c0979cbe931076
Haisheng W - M <haisheng@mercury.com>
2023-12-07 00:02:03 -0800

    day3 part2

--- src/Day3.hs
+++ src/Day3.hs
@@ -7,21 +7,20 @@ import Data.Vector (Vector, (!?))
 import Data.Vector qualified as V
 import Test.Hspec
 
-go :: IO ()
-go =
+go1 :: IO ()
+go1 =
     do
         readFile "data/day3-input.txt"
         >>= print
-        . goInternal
+        . goInternal1
         . lines
 
--- debug = do
---     rawInputs <- lines <$> readFile "data/day3-input.txt"
---     let inputs = V.fromList (fmap V.fromList rawInputs)
---     writeFile "/tmp/2.txt" (unlines $ fmap show $ sort $ findNumber inputs $ findDigitsIndex inputs)
+-------------------------------------------------------------------------------
+--                                   Part1                                   --
+-------------------------------------------------------------------------------
 
-goInternal :: [[Char]] -> Int
-goInternal rawInputs =
+goInternal1 :: [[Char]] -> Int
+goInternal1 rawInputs =
     let inputs = V.fromList (fmap V.fromList rawInputs)
      in sum $ findNumber inputs $ findDigitsIndex inputs
 
@@ -75,7 +74,7 @@ removeIndexesForSameNumber xs =
         [] -> []
         [x] -> [x]
         (x : y : rest) ->
-            [x | not (fst x == fst x && snd y - snd x == 1)]
+            [x | not (fst x == fst y && snd y - snd x == 1)]
                 ++ removeIndexesForSameNumber (y : rest)
 
 findNumber :: Vector (Vector Char) -> [(Int, Int)] -> [Int]
@@ -104,6 +103,63 @@ findNumberAtIndexRight inputs (i, j) accum =
         Nothing -> accum
 
 -------------------------------------------------------------------------------
+--                                   Part2                                   --
+-------------------------------------------------------------------------------
+
+go2 :: IO ()
+go2 =
+    do
+        readFile "data/day3-input.txt"
+        >>= print
+        . goInternal2
+        . lines
+
+goInternal2 :: [[Char]] -> Int
+goInternal2 rawInputs =
+    let inputs = V.fromList (fmap V.fromList rawInputs)
+     in sum $ V.map product $ findGearNumber inputs
+
+findGearNumber :: Vector (Vector Char) -> Vector [Int]
+findGearNumber inputs =
+    V.map sort
+        $ V.filter (\xs -> length xs == 2)
+        $ fmap
+            ( findNumber inputs
+                . removeIndexesForSameNumber
+                . sort
+                . filter
+                    ( \(i, j) -> case inputs !? i of
+                        Just row -> case row !? j of
+                            Just e -> not (isDot e)
+                            Nothing -> False
+                        Nothing -> False
+                    )
+            )
+        $ V.filter (not . null)
+        $ V.concat
+        $ V.toList
+        $ V.imap
+            ( \i v ->
+                V.imap
+                    ( \j a ->
+                        if a == '*'
+                            then
+                                [ (i - 1, j)
+                                , (i - 1, j - 1)
+                                , (i - 1, j + 1)
+                                , (i + 1, j)
+                                , (i + 1, j - 1)
+                                , (i + 1, j + 1)
+                                , (i, j - 1)
+                                , (i, j + 1)
+                                ]
+                            else []
+                    )
+                    v
+            )
+            inputs
+
+-------------------------------------------------------------------------------
 --                                    Test                                   --
 -------------------------------------------------------------------------------
 
@@ -112,30 +168,43 @@ testMain = hspec $ do
     describe "Day3" $ do
         let testDataWithIndex =
                 zipWith
-                    (\(a, b) i -> (a, b, i))
+                    (\(a, b, c) i -> (a, b, c, i))
                     testData
                     ([1 ..] :: [Int])
-        forM_ testDataWithIndex $ \(inputs, expected, index) -> do
+        forM_ testDataWithIndex $ \(inputs, expected1, expected2, index) -> do
             it (show index) $ do
-                goInternal inputs `shouldBe` expected
+                goInternal1 inputs `shouldBe` expected1
+                goInternal2 inputs `shouldBe` expected2
 
-testData :: [([String], Int)]
+testData :: [([String], Int, Int)]
 testData =
-    [ (sampleData, 4361)
-    , (["*67..114.."], 67)
+    [ (sampleData, 4361, 467835)
+    , (["*67..114.."], 67, 0)
     ,
         (
             [ "67...114.."
             , "+........."
             ]
         , 67
+        , 0
         )
     ,
         (
-            [ "&....114.."
-            , ".8........"
+            [ "..9..114.."
+            , ".*........"
+            , ".81........"
             ]
-        , 8
+        , 90
+        , 729
+        )
+    ,
+        (
+            [ "..9..114.."
+            , ".*5......."
+            , ".81........"
+            ]
+        , 95
+        , 0
         )
     ,
         (
@@ -143,8 +212,18 @@ testData =
             , ".8.....+.."
             ]
         , 16
+        , 0
         )
-    , (["..658.509*378.."], 887)
+    ,
+        (
+            [ "362..1......*"
+            , ".....*......3"
+            , "......605...."
+            ]
+        , 609
+        , 605
+        )
+    , (["..658.509*378.."], 887, 192402)
     ]
 
 sampleData :: [String]
