1d0b89ba55cca1b2e00df12d9430e9f647d8d709
Haisheng W - M <haisheng@mercury.com>
2023-12-16 11:33:02 -0800

    Day15 part2 - not working

--- 2023/src/Day15.hs
+++ 2023/src/Day15.hs
@@ -4,10 +4,14 @@ module Day15 where
 
 import Control.Monad
 import Data.Char
+import Data.Function
+import Data.List
 import Data.Text (Text)
 import Data.Text qualified as T
 import Data.Text.IO qualified as T
 import Test.Hspec
+import Text.Parsec
+import Text.Parsec.String (Parser)
 
 -- import Debug.Trace
 
@@ -16,13 +20,6 @@ goInternalWithFile fn =
     T.readFile "data/day15-input.txt"
         >>= print
             . fn
-
-run :: Text -> Int
-run =
-    sum
-        . fmap hash
-        . T.splitOn ","
-            . T.init -- ignore line break
 
 hash :: Text -> Int
 hash = T.foldl' hashInternal 0
@@ -36,7 +33,86 @@ hashInternal initVal c =
 -------------------------------------------------------------------------------
 
 go1 :: IO ()
-go1 = goInternalWithFile run
+go1 = goInternalWithFile run1
+
+run1 :: Text -> Int
+run1 =
+    sum
+        . fmap hash
+        . T.splitOn ","
+        . T.init -- ignore line break
+
+-------------------------------------------------------------------------------
+--                                   Part 2                                  --
+-------------------------------------------------------------------------------
+
+type Label = Text
+type FocalLength = Int
+data Printer = Remove Label | Add Label FocalLength
+    deriving (Show)
+
+printerHasSameLabel :: Printer -> Printer -> Bool
+printerHasSameLabel (Remove l1) (Remove l2) = l1 == l2
+printerHasSameLabel (Remove l1) (Add l2 _) = l1 == l2
+printerHasSameLabel (Add l1 _) (Remove l2) = l1 == l2
+printerHasSameLabel (Add l1 _) (Add l2 _) = l1 == l2
+
+boxNumber :: Printer -> Int
+boxNumber (Remove l1) = hash l1
+boxNumber (Add l1 _) = hash l1
+
+parseSingle :: Text -> Printer
+parseSingle input =
+    case parse printerParser "" (T.unpack input) of
+        Left err -> error (show err)
+        Right r -> r
+
+printerParser :: Parser Printer
+printerParser = try parseRemoveOp <|> parseAddOp
+  where
+    parseRemoveOp = do
+        label1 <- many1 lower
+        _ <- char '-'
+        pure $ Remove (T.pack label1)
+    parseAddOp = do
+        label2 <- many1 lower
+        _ <- char '='
+        focalL <- many1 digit
+        pure $ Add (T.pack label2) (read focalL)
+
+run2 :: Text -> Int
+run2 =
+    sum
+        . concatMap calculateFocalLength
+        . fmap (\xs -> (fst $ head xs, printOnBoxes $ fmap snd xs))
+        . groupBy ((==) `on` fst)
+        . sortBy (compare `on` fst)
+        . fmap (\p -> (boxNumber p, p))
+        . fmap parseSingle
+        . T.splitOn ","
+        . T.init -- ignore line break
+
+-- do computation for list of printer on same box
+printOnBoxes :: [Printer] -> [Printer]
+printOnBoxes = foldl' go []
+  where
+    go :: [Printer] -> Printer -> [Printer]
+    go [] p = [p]
+    go (x : rest) p =
+        if printerHasSameLabel p x
+            then case p of
+                Remove _ -> rest
+                Add _ _ -> p : rest
+            else x : go rest p
+
+calculateFocalLength :: (Int, [Printer]) -> [Int]
+calculateFocalLength (boxNum, xs) = uncurry calc <$> zip xs [1 ..]
+  where
+    calc (Add _ f) index = (boxNum + 1) * f * index
+    calc (Remove _) _ = 0
+
+go2 :: IO ()
+go2 = goInternalWithFile run2
 
 -------------------------------------------------------------------------------
 --                                    Test                                   --
@@ -51,13 +127,14 @@ testMain = do
                         (\(a, b, c) i -> (a, b, c, i))
                         testData
                         ([1 ..] :: [Int])
-            forM_ testDataWithIndex $ \(inputs, expected1, _expected2, index) -> do
+            forM_ testDataWithIndex $ \(inputs, expected1, expected2, index) -> do
                 it (show index) $ do
-                    run inputs `shouldBe` expected1
+                    run1 inputs `shouldBe` expected1
+                    run2 inputs `shouldBe` expected2
 
 testData :: [(Text, Int, Int)]
 testData =
-    [ (sample, 1320, 0)
+    [ (sample, 1320, 145)
     ]
 
 sample :: Text
